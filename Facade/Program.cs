09331a5980d5d9b3dd9170cac530ad5da31c4bc5
Sergio Vegas Alonso <sergio.vegas.alonso@gmail.com>
2023-07-20 19:05:31 +0200

    Facade in alfa version

---
+++ Facade/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace Facade;
+
+/*
+ * Owner is the Facade class
+ */
+internal class Contractor
+{
+    internal Contractor() { }
+
+    internal void PerformAlteration() { }
+}
+
+/*
+ * Owner is the Director class
+ */
+internal class Owner
+{
+    private readonly Bank _bank;
+
+    private readonly Townhall _townhall;
+
+    private readonly Contractor _contractor;
+
+    internal Owner()
+    {
+        _bank = new Bank();
+        _townhall = new Townhall();
+        _contractor = new Contractor();
+    }
+
+    internal void PerformAlteration()
+    {
+        _bank.ArrangeLoan();
+        _townhall.ArrangeLicense();
+        _contractor.PerformAlteration();
+    }
+}
+
+internal class Townhall
+{
+    internal Townhall() { }
+
+    internal void ArrangeLicense() { }
+}
+
+internal class Bank
+{
+    internal Bank() { }
+
+    internal void ArrangeLoan() { }
+}
+
+internal class Carpenter
+{
+    internal Carpenter() { }
+
+    internal void InstallWindow() { } 
+
+    internal void InstallDoor() { }
+}
+
+internal class Painter
+{
+    internal Painter() { }
+
+    internal void RemoveOldPaint() { }
+
+    internal void ApplyPaint() { }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+    }
+}
+
